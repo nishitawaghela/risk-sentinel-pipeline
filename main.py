2af1b39a9c4b14c07fe63875893577b0ce8f5939
Nishita <waghelanishita@gmail.com>
2026-02-15 12:02:21 +0530

    added POST Method

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
-@app.get("/")
+ # This is a simple API endpoint that returns a JSON response indicating that the Sentinel service is online (GET request to the root URL "/"). 
 def read_root():
     return {"status": "Sentinel Online", "key": "Security"}
+
+#POST request to "/trades/" that accepts a JSON payload representing a trade. The payload must match the structure defined by the Trade class. If the incoming data is valid according to the Trade schema, the create_trade function will return a JSON response confirming that the trade was received along with the data that was sent.
+
+from pydantic import BaseModel # Import the "Data Validator"
+
+# 1. Define the "Order Form" (Schema)
+# This tells the Waiter EXACTLY what a valid trade looks like.
+class Trade(BaseModel):
+    trade_id: str
+    stock_ticker: str
+    price: float
+    quantity: int
+
+# 2. Create the POST Door
+@app.post("/trades/")
+def create_trade(trade: Trade):
+    # This function only runs if the data matches the structure above.
+    return {"message": "Trade received", "data": trade}
